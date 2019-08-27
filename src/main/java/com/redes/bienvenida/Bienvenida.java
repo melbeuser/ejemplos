c1acec713d5e941e5a6bdee9eef33c0029b419df
melin <melin@DESKTOP-MJJLAIG>
2019-08-27 11:15:40 -0500

    Primer programa de Bienvenida con versión GIT

---
+++ src/main/java/com/redes/bienvenida/Bienvenida.java
@@ -0,0 +1,17 @@
+/*
+ * Melissa Blanqueto
+ * 27 Ago 2019
+ * Programa que muestra un mensaje de bienvenida
+ *
+ */
+package com.redes.bienvenida;
+
+/**
+ *
+ * @author melissabe
+ */
+public class Bienvenida {
+    public static void main(String[] args) {
+        System.out.println("Bienvenidos a Java");
+    }
+}
